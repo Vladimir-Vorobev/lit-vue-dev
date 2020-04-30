eb85e7916aa29a96071436a8429cd32c7c10b07e
Vladimir <vovav2005@yandex.ru>
2020-04-30 11:49:15 +0300

    1

--- HomeWork-2/src/components/AllEvents.vue
+++ HomeWork-2/src/components/AllEvents.vue
@@ -1,34 +1,39 @@
 <template>
-    <div id="text"></div>
+    <div class="main">{{data}}</div>
 </template>
 
 <script>
+import needle from "needle";
+import cheerio from "cheerio";
 export default {
     name: 'AllEvents',
+    data(){
+        var url = "http://events.mosedu.ru/";
+        let data
+        let Info = function(){
+            needle.get(url,function(err,res){
+                if(err) throw(err);
+
+                var $ = cheerio.load(res.body);
+
+                data = $(".event-block-blocks").text().split(/\s* \s* \s*/)
+            });
+            console.log(data)
+            return{
+                data
+            }
+        }
+        
+        console.log(Info())
+        return{
+           data
+        }
+    }
 }
-let needle = require("needle");
-let cheerio= require("cheerio");
-
-let url = "http://events.mosedu.ru/";
-
-needle.get(url,function(err,res){
-  if(err) throw(err);
-
-  let $ = cheerio.load(res.body);
-  let string = $('.event-block-blocks').text()
-  let re = /\s* \s* \s*/
-  let data = string.split(re)
-  for(let i = 1; i < data.length; i+=6){
-      console.log('Дата мероприятия: ' + data[i])
-      console.log('Время мероприятия: ' + data[i + 1])
-      console.log('Свободные места: ' + data[i + 2])
-      console.log('Для кого: ' + data[i + 3])
-      console.log('Форма мероприятия: ' + data[i + 4])
-      console.log('Название мероприятия: ' + data[i + 5])
-  }
-});
 </script>
 
 <style scoped>
-
+.main{
+	padding-top: 260px;
+}
 </style>
