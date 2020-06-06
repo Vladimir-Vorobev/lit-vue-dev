c99c842ebea601e3ba23d7d2cb1d7301c58bdf69
Vladimir <vovav2005@yandex.ru>
2020-06-06 15:57:07 +0300

    Update FriendStatistics.vue

--- lit-vue-dev-feature/HomeWork-2/src/components/FriendStatistics.vue
+++ lit-vue-dev-feature/HomeWork-2/src/components/FriendStatistics.vue
@@ -1,17 +1,33 @@
 <template>
     <div class='main container'>
-            <h2>Статистика</h2>
-            <select class="custom-select custom-select-sm mb-3 events" onchange="location.href=this.value">
-                <option value="/friend-statistics" selected>Статистика друзей</option>
-                <option value="/school-statistics">Обобщенная статистика школы (только для сотрудников учебных заведений)</option>
-                <option value="/full-school-statistics">Полная статистика школы (только для сотрудников учебных заведений)</option>
-            </select>
-            <div class="form-group row">
-              <div class="col-12 col-md-8"><input name="code" class="form-control name" placeholder="Код от Вашего друга"></div>
-              <div class="col-12 col-md-4" style="padding: 0px 15px;"><button class="btn btn-outline-success" style="width: 100%;" @click="addFriend()">Добавить друга</button></div>
+        <h2>Статистика</h2>
+        <select class="custom-select custom-select-sm mb-3 events" onchange="location.href=this.value">
+            <option value="/friend-statistics" selected>Статистика друзей</option>
+            <option value="/school-statistics">Обобщенная статистика школы (только для сотрудников учебных заведений)</option>
+            <option value="/full-school-statistics">Полная статистика школы (только для сотрудников учебных заведений)</option>
+        </select>
+        <div class="form-group row">
+            <div class="col-12 col-md-8"><input name="code" class="form-control name" placeholder="Код от Вашего друга"></div>
+            <div class="col-12 col-md-4" style="padding: 0px 15px;"><button class="btn btn-outline-success" style="width: 100%;" @click="addFriend()">Добавить друга</button></div>
+        </div>
+        <div class="form-group row code"></div>
+        <div class="student" v-for="item in data" :key="item.value">
+            <div class="si row">
+                <div class="col-12 col-md-6"><span class="ns">{{item.name}} {{item.surname}}</span></div>
+                <div class="col-6 col-md-3">Класс: <span class="numc">{{item.class_number}}</span></div>
+                <div class="col-6 col-md-3">Символ: <span class="simvol">{{item.simvol}}</span></div>
             </div>
-            <div class="form-group row code">
+            <div v-if="item.checkedEvents.length == 0">Нигде не участвует <br></div>
+            <div class="card" v-else v-for="item2 in item.checkedEvents" :key="item2.value">
+                <div class="card-header">{{item2.date}}</div>
+                <div class="card-body">
+                    <h5 class="card-title">{{item2.name}}</h5>
+                    <p class="card-text"><i class="far fa-clock"></i> {{item2.time}}</p>
+                    <p class="card-text">Тип: {{item2.type}}</p>
+                    <a :href="item2.link" class="btn btn-primary">Перейти к мероприятию</a>
+                </div>
             </div>
+        </div>
     </div>
 </template>
 
@@ -19,7 +35,13 @@
 import needle from 'needle'
 export default {
     name: 'Statistics',
-    mounted(){
+    data(){
+        let data = []
+        return{
+            data
+        }
+    },
+    beforeMount(){
         let email = this.$store.getters.email
         fetch('https://makual.ru/api/getCodeInformation', {
             method: 'get',
@@ -30,28 +52,7 @@ export default {
             return response.json()
         })
         .then(data => {
-            document.querySelector('.main').insertAdjacentHTML(
-                    'beforeEnd',
-                    '<style> .card{ margin-top: 10px !important; } .card-body { text-align: left !important; } .card-body h5{ font-weight: bold; } .student{margin-top: 2.5em;border: 1.5px solid #818181; /* Белая рамка */border-radius: 10px; /* Радиус скругления */padding: 1.2em;}.si{font-size: 1.5em;margin-bottom: 1.5em;}.stat_school{margin-top: 1.5em;}.rate{text-align: left;font-size: 1.3em;margin-bottom: 2em;}.numr{font-size: 1.5em !important;font-weight: bold;}.colv{margin-left: 1.5em;}',
-            )
-            for(let i = 0; i < data.length; i++){
-                document.querySelector('.main').insertAdjacentHTML(
-                    'beforeEnd',
-                    '<div class="student ' + data[i].name+data[i].surname+ ' "> <div class="si row"> <div class="col-12 col-md-6"><span class="ns">' + data[i].name + ' ' + data[i].surname + '</span></div> <div class="col-6 col-md-3">Класс: <span class="numc">' + data[i].class_number + '</span></div> <div class="col-6 col-md-3">Символ: <span class="simvol">' + data[i].simvol + '</span></div> </div>',
-                )
-                if(data[i].checkedEvents.length == 0){
-                    document.querySelector('.'+data[i].name+data[i].surname+'').insertAdjacentHTML(
-                        'beforeEnd',
-                        'Нигде не участвует <br>'
-                    )
-                }
-                for(let m = 0; m < data[i].checkedEvents.length; m++){
-                    document.querySelector('.'+data[i].name+data[i].surname+'').insertAdjacentHTML(
-                        'beforeEnd',
-                        '<div class="card">  <div class="card-header " style="font-weight: bold;">' + data[i].checkedEvents[m].date + '</div> <div class="card-body"> <h5 class="card-title">' + data[i].checkedEvents[m].name + '</h5> <p class="card-text"><i class="far fa-clock"></i>' + ' ' + data[i].checkedEvents[m].time + '</p> <p class="card-text">' + 'Тип: ' + data[i].checkedEvents[m].type + '</p> <a href=' + data[i].checkedEvents[m].link +  ' class="btn btn-primary">Перейти к мероприятию</a> </div> </div>',
-                    )
-                }
-            }
+            this.data = data
         })
         .catch(err => {
             console.log(err)
@@ -93,5 +94,42 @@ export default {
     padding: 30px;
     min-height: 100vh;
     margin-bottom: 0px;
+}
+.card{ 
+    margin-top: 10px !important;
+}
+.card-body{
+    text-align: left !important;
+}
+.card-header{
+    font-weight: bold;
+}
+.card-body h5{
+    font-weight: bold;
+}
+.student{
+    margin-top: 2.5em;border: 1.5px solid #818181;
+    /* Белая рамка */
+    border-radius: 10px;
+    /* Радиус скругления */
+    padding: 1.2em;
+}
+.si{
+    font-size: 1.5em;margin-bottom: 1.5em;
+}
+.stat_school{
+    margin-top: 1.5em;
+}
+.rate{
+    text-align: left;
+    font-size: 1.3em;
+    margin-bottom: 2em;
+}
+.numr{
+    font-size: 1.5em !important;
+    font-weight: bold;
+}
+.colv{
+    margin-left: 1.5em;
 }
 </style>
