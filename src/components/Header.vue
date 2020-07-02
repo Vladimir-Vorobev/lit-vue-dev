86a6e6777fa25bdee657e05bdfa9228fc4df4312
SerPan23 <igracom@yandex.ru>
2020-07-02 20:15:06 +0300

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -53,7 +53,7 @@
           </router-link>
           <div class="dropdown dropleft" style="display:none">
             <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <router-link to="/profile" type="button" class="btn btn-primary btn-lg" ref="login">{{loginText}}</router-link>
+              {{loginText}}
             </button>
             <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
               <a class="dropdown-item" href="#">Моя страница</a>
